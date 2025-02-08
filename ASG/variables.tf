ef2c34f29d357ffaf6e0bbfe2848aea7600bee17
Ahmed Alaa <ahmedalaa24683901@gmail.com>
2025-02-08 20:45:51 +0200

    Add variables for ASG and network module outputs

---
+++ ASG/variables.tf
@@ -0,0 +1,48 @@
+variable "vpc_id" {
+  type = string
+}
+variable "ami" {
+ 
+    type = string
+  
+}
+variable "elb_id" {
+  type = string
+}
+
+variable "public_ec2_tags" {
+
+    type = list(string)  
+}
+
+variable "private_ec2_tags" {
+    type = list(any)  
+}
+
+variable "lb_name" {
+  type = string  
+}
+
+variable "lb_type" {
+  type = string  
+}
+
+variable "AZ" {
+  type    = list(string)
+}
+
+
+variable "public_subnets_id" {
+  type = list(string)
+}
+variable "private_subnets_id" {
+  type = list(string)
+}
+
+variable "public_SG_EC2s" {
+  type = string
+}
+
+variable "private_SG_EC2s" {
+  type = string
+}
